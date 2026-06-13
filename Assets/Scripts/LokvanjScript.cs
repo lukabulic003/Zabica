5203e4ff03d09399f042465ca71c7005e5fb731a
lukabulic003 <luka.bulic003@gmail.com>
2026-06-14 01:16:15 +0200

    Kolizije

--- Assets/Scripts/LokvanjScript.cs
+++ Assets/Scripts/LokvanjScript.cs
@@ -6,28 +6,51 @@ public class LokvanjScript : MonoBehaviour
     public int potrebanBrojZaPad = 2;
     public float brzinaPada = 2f;
     private bool pada = false;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log(gameObject.name + " - ENTER kolizija sa: " + other.gameObject.name + " (tag: " + other.gameObject.tag + ")");
+
         if (other.gameObject.CompareTag("Lotus"))
         {
             lotusiNaPlatformi.Add(other.collider);
         }
+
+        if (other.gameObject.CompareTag("Wather"))
+        {
+            Debug.Log(gameObject.name + " - dodirnuo VODU, zaustavljam padanje");
+            pada = false;
+        }
     }
+
     void OnCollisionExit2D(Collision2D other)
     {
         Debug.Log(gameObject.name + " - EXIT kolizija sa: " + other.gameObject.name);
+
         if (other.gameObject.CompareTag("Lotus"))
         {
             lotusiNaPlatformi.Remove(other.collider);
         }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(gameObject.name + " - TRIGGER ENTER sa: " + other.gameObject.name + " (tag: " + other.gameObject.tag + ")");
+
+        if (other.gameObject.CompareTag("Voda"))
+        {
+            Debug.Log(gameObject.name + " - dodirnuo VODU (trigger), zaustavljam padanje");
+            pada = false;
+        }
+    }
+
     void Update()
     {
         if (lotusiNaPlatformi.Count >= potrebanBrojZaPad)
         {
             pada = true;
         }
+
         if (pada)
         {
             transform.position += Vector3.down * brzinaPada * Time.deltaTime;
